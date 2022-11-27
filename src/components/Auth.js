e9cca638ee26a45552f132e464bca6a7d291f308
Yazeed al-Momani <yazeed.02.momani@outlook.com>
2022-11-27 18:16:24 +0300

    implement login

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,19 +1,35 @@
-import classes from './Auth.module.css';
+import { useDispatch } from "react-redux";
+import { actions } from "../store/redux";
+import classes from "./Auth.module.css";
 
 const Auth = () => {
+  const authDispatcher = useDispatch();
+
+  const btnHandler = function (e) {
+    e.preventDefault();
+
+    authDispatcher(actions.auth.logIn());
+  };
+
   return (
     <main className={classes.auth}>
       <section>
         <form>
           <div className={classes.control}>
-            <label htmlFor='email'>Email</label>
-            <input type='email' id='email' />
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+            />
           </div>
           <div className={classes.control}>
-            <label htmlFor='password'>Password</label>
-            <input type='password' id='password' />
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+            />
           </div>
-          <button>Login</button>
+          <button onClick={btnHandler}>Login</button>
         </form>
       </section>
     </main>
