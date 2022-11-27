e9744909a698c4ac1e07147f89bb5e7f95a63212
Yazeed al-Momani <yazeed.02.momani@outlook.com>
2022-11-27 17:47:56 +0300

    add form and nav

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import Counter from './components/Counter';
-
+import Counter from "./components/Counter";
+import { Fragment } from "react";
+import Auth from "./components/Auth";
+import Header from "./components/Header";
+import UserProfile from "./components/UserProfile";
 
 function App() {
   return (
-    <Counter />
+    <Fragment>
+      <Header />
+      <Auth />
+      <Counter />
+    </Fragment>
   );
 }
 
